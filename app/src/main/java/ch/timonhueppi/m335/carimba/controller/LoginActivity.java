50ca795ce13873e28f5160ad55e88de9c5105905
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-27 22:24:29 +0100

    Adds layout of LoginActivity

---
+++ app/src/main/java/ch/timonhueppi/m335/carimba/controller/LoginActivity.java
@@ -0,0 +1,16 @@
+package ch.timonhueppi.m335.carimba.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.timonhueppi.m335.carimba.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/ch/timonhueppi/m335/carimba/controller/MainActivity.java
+++ app/src/main/java/ch/timonhueppi/m335/carimba/controller/MainActivity.java
@@ -1,8 +1,11 @@
-package ch.timonhueppi.m335.carimba;
+package ch.timonhueppi.m335.carimba.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import ch.timonhueppi.m335.carimba.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
