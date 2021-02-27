f318602f28fa351acd1ef457ba95f23e9168c68d
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-27 22:37:43 +0100

    Adds layout of SignUpActivity

---
+++ app/src/main/java/ch/timonhueppi/m335/carimba/controller/SignUpActivity.java
@@ -0,0 +1,16 @@
+package ch.timonhueppi.m335.carimba.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.timonhueppi.m335.carimba.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
