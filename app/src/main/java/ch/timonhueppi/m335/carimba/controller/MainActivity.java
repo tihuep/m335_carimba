4e856d58cbccff2d5e044ee28f6de564a08cbfff
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-28 01:02:40 +0100

    Adds initial auto-routing on login-state

--- app/src/main/java/ch/timonhueppi/m335/carimba/controller/MainActivity.java
+++ app/src/main/java/ch/timonhueppi/m335/carimba/controller/MainActivity.java
@@ -2,20 +2,76 @@ package ch.timonhueppi.m335.carimba.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 import ch.timonhueppi.m335.carimba.R;
+import ch.timonhueppi.m335.carimba.service.UserService;
 
 public class MainActivity extends AppCompatActivity {
+
+    UserService userService;
+    boolean serviceBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginActivity.class);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, UserService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        serviceBound = false;
+    }
+
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            UserService.LocalBinder binder = (UserService.LocalBinder) service;
+            userService = binder.getService();
+            serviceBound = true;
+
+            //put actions here
+            userService.initFirebaseAuth();
+            startActivity();
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            serviceBound = false;
+        }
+    };
+
+    private void startActivity(){
+        Intent intent;
+        if (userService.loggedIn())
+            intent = new Intent(this, CarsActivity.class);
+        else
+            intent = new Intent(this, LoginActivity.class);
+
         startActivity(intent);
         finish();
     }
+
 }
