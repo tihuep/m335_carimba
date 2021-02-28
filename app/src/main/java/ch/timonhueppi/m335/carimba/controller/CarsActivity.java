f9a63c20afc41bd32a93cae36dce3d234aa9d0e1
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-28 01:18:04 +0100

    Adds Logout-functionality

--- app/src/main/java/ch/timonhueppi/m335/carimba/controller/CarsActivity.java
+++ app/src/main/java/ch/timonhueppi/m335/carimba/controller/CarsActivity.java
@@ -2,15 +2,78 @@ package ch.timonhueppi.m335.carimba.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Button;
 
 import ch.timonhueppi.m335.carimba.R;
+import ch.timonhueppi.m335.carimba.service.UserService;
 
 public class CarsActivity extends AppCompatActivity {
+
+
+    UserService userService;
+    boolean serviceBound = false;
+
+    Button btnCarsLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars);
+
+        btnCarsLogout = findViewById(R.id.btnCarsLogout);
+        setButtonHandlers();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, UserService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        serviceBound = false;
+    }
+
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            UserService.LocalBinder binder = (UserService.LocalBinder) service;
+            userService = binder.getService();
+            serviceBound = true;
+
+            //put actions here
+            userService.initFirebaseAuth();
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            serviceBound = false;
+        }
+    };
+
+    private void setButtonHandlers(){
+        btnCarsLogout.setOnClickListener(v -> {
+            userService.logout();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
