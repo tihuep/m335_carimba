21b1400e13384a41663070de564eca34eaaf56fd
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-28 14:08:36 +0100

    Adds Toolbar Menu

--- app/src/main/java/ch/timonhueppi/m335/carimba/controller/CarsActivity.java
+++ app/src/main/java/ch/timonhueppi/m335/carimba/controller/CarsActivity.java
@@ -1,6 +1,7 @@
 package ch.timonhueppi.m335.carimba.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.ComponentName;
 import android.content.Context;
@@ -8,6 +9,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import ch.timonhueppi.m335.carimba.R;
@@ -25,9 +28,41 @@ public class CarsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        if (myToolbar != null) {
+            setSupportActionBar(myToolbar);
+        }
+
 
         btnCarsLogout = findViewById(R.id.btnCarsLogout);
         setButtonHandlers();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.btnMenuSearch:
+                return true;
+            case R.id.btnMenuLogout:
+                // This is a method, that performs logging user out
+                userService.logout();
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            // Here could be other cases, if you added more than one menu
+            // item
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
